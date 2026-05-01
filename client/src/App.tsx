4da67bfe3b02bee245e327cf40f99491c29ad9cc
Manu Krishna <krishnamanu27112004@gmail.com>
2026-05-01 10:11:11 +0530

    chores: Initialized a MERN environment and folder structure.

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      Tailwind v4 working
+    </div>
+  );
+}
+
+export default App;
