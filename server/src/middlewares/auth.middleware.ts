e9d9a629e0f426924d0ebf6b882e9bf6d1c3b1cc
Manu Krishna <krishnamanu27112004@gmail.com>
2026-05-05 13:29:53 +0530

    fix: Refactored the authentication middleware for the application.

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -1,6 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+type JwtPayload = {
+    id: string;
+}
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    (req as any).user = { id: "000000000000000000000001" };
-    next();
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ success: false, message: "Unauthorzied: No token provided" });
+    }
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+        (req as any).user = { id: decoded.id };
+        next();
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Unauthorized: Invalid token", });
+    }
 }
